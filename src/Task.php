abd6688cc3811b4a4c793345267d10b41b1a9b41
rodrigopepato <rodrigo.pepato@gmail.com>
2024-11-21 17:19:10 -0300

    Add methods to retrieve and update tasks in the Task model

--- src/Task.php
+++ src/Task.php
@@ -39,6 +39,25 @@ class Task
     }
 
 
+    public function getTaskById($id)
+    {
+        $query = "SELECT * FROM tasks WHERE id = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_assoc();
+    }
+
+    public function updateTask($id, $description)
+    {
+        $query = "UPDATE tasks SET description = ? WHERE id = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("si", $description, $id);
+        return $stmt->execute();
+    }
+
+
     public function deleteTask($id)
     {
         $stmt = $this->db->prepare("DELETE FROM tasks WHERE id = ?");
